ce6becc594365fce45a216654bc843ae38de10f2
MANISH SINGH PARIHAR <manishsparihar2020@gmail.com>
2023-12-08 20:09:53 +0530

    minor changes

--- test.py
+++ test.py
@@ -1,22 +1,19 @@
+from openai import OpenAI
 import os
-import openai
+from dotenv import load_dotenv
 
-# Load your API key from an environment variable or secret management service
-openai.api_key = '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'
+load_dotenv()
 
-messages = [
-    {"role": "system", "content": "You are a kind helpful assistant."},
-]
-while True:
-    message = input("User : ")
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+client = OpenAI(
+    api_key = os.getenv("OPENAI_API_KEY")
+)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print("completion.choices[0].message")
